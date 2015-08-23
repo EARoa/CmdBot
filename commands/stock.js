3236922d9a0bbfa98ced483796af20fb47f55fec
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-08-23 05:15:31 +0100

    Added stock command

--- commands/stock.js
+++ commands/stock.js
@@ -1,16 +1,40 @@
-var http         = require('http');
-var yahooFinance = require('yahoo-finance');
+var http       = require('http');
 
 exports.handleCommand = function(bot, type, msg, callback) {
     //Generate the details view
     var chatID = msg.chat.id;
 
-    console.log(msg.args)
+    if(msg.args.length < 1) {
+        bot.sendMessage(chatID,  "Oops, I think you forgot to give me a stock symbol. :/");
+        return callback(null);
+    }
 
-    yahooFinance.snapshot({
-      symbol: 'AAPL',
-      fields: ['s', 'n', 'd1', 'l1', 'c1'],
-    }, function (err, snapshot) {
-      console.log(snapshot)
+    var symbol = msg.args[0];
+
+    http.get({
+        host: 'query.yahooapis.com',
+        path: '/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20=%22' + symbol + '%22&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env'
+    },
+    function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) { body += d; });
+        response.on('end', function() {
+            // parse as JSON
+            var data = JSON.parse(body).query.results.quote;
+
+            if (data.StockExchange === null) {
+                bot.sendMessage(chatID,  symbol + " is not a valid ticker symbol!", {reply_to_message_id: msg["message_id"]});
+                return callback(null);
+            }
+
+            var message_txt = "The current stock data for " + symbol + " is \n" +
+                              "Latest Price: " + data.LastTradePriceOnly + ", " + (data.Change < 0 ? "down" : "up") + " by " + data.Change + " points\n" +
+                              "Last Trade: " + data.LastTradeDate + ' ' + data.LastTradeTime + "\n" +
+                              "Stock Exchange: " + (data.StockExchange).replace("NMS", "NASDAQ").toUpperCase() + "\n"
+
+            bot.sendMessage(chatID, message_txt, {reply_to_message_id: msg["message_id"]});
+            return callback(null);
+        });
     });
 };
