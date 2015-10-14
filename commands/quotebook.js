42b617fa8b45b407184f1e7fb32f31b0f7b6557f
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-10-14 09:04:38 +0100

    Modified a few taglines

--- commands/quotebook.js
+++ commands/quotebook.js
@@ -2,7 +2,7 @@ exports.handleCommand = function(bot, type, msg, callback) {
     //Generate the details view
     var chatID = msg.chat.id;
 
-    bot.sendMessage(chatID, "docs.google.com/document/d/1AiAED9Vhv_Y8ED58itnFvIpriCrhKqV82-HjvS1toac/view");
+    bot.sendMessage(chatID, "The Quotebook:\ndocs.google.com/document/d/1AiAED9Vhv_Y8ED58itnFvIpriCrhKqV82-HjvS1toac/view");
 
     return callback(null);
 };
