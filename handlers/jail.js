1eeb2e878ec9631f70bade50d6ae18d6f303606f
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-10-14 08:28:00 +0100

    Added Eytan's Jail Handler and fixed some incompatibilities

--- handlers/jail.js
+++ handlers/jail.js
@@ -2,7 +2,13 @@ exports.handleCommand = function(bot, type, msg, callback) {
     //Grab the chat ID
     var chatID = msg.chat.id;
 
-    if(msg.text.split(" ").toLowerCase().indexOf("jail") > -1 || msg.text.split(" ").toLowerCase().indexOf("prison") > -1) {
+    if(!msg.text) {
+        return;
+    }
+
+    msg.text = msg.text.toLowerCase()
+
+    if(msg.text.indexOf("jail") > -1 || msg.text.indexOf("prison") > -1) {
         bot.sendMessage(chatID, "^(school)", {reply_to_message_id: msg["message_id"]});
         return callback(true);
     }
